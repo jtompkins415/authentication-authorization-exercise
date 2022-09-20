75e331163b450f7fb117342e893d91cd7d0b4328
jtompkins415 <john.tompkins415@gmail.com>
2022-09-19 17:28:26 -0700

    create form for login

--- forms.py
+++ forms.py
@@ -10,3 +10,9 @@ class UserRegistration(FlaskForm):
     email = StringField('E-Mail', validators=[InputRequired()])
     first_name = StringField('First Name', validators=[InputRequired()])
     last_name = StringField('Last Name', validators=[InputRequired()])
+
+class LoginForm(FlaskForm):
+    '''Handle User Login'''
+
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
